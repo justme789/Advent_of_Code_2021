d7063e90a76e3f8eaddd26ead55f704b048c9d5f
Abouelseoud, Ibrahim (k1893464) <nobodyxoxo123@gmail.com>
2021-12-06 23:17:25 +0000

    Day 6 Solution

---
+++ AoC_6.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AoC_6 {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Integer> fishies = new ArrayList<Integer>();
+        long[] fish = new long[10];
+        Scanner textfile = new Scanner(new File("aoc.txt"));
+        while (textfile.hasNextLine()) {
+            String line = textfile.nextLine();
+            list.add(line);
+        }
+        System.out.println(list.toString());
+        for (int i = 0; i < list.get(0).length() - 1; i++) {
+            int indexOfComma = list.get(0).indexOf(",", i);
+            fishies.add(Integer.parseInt(list.get(0).substring(i, indexOfComma)));
+            i = indexOfComma;
+        }
+        fishies.add(Integer.parseInt(list.get(0).substring(list.get(0).lastIndexOf(",") + 1, list.get(0).length())));
+        int days = 0;
+        for (int i : fishies) {
+            fish[i]++;
+        }
+        while (days < 256) {
+            fish[9] += fish[0];
+            fish[7] += fish[0];
+            fish[0] = 0;
+            fish[0] = fish[1];
+            fish[1] = fish[2];
+            fish[2] = fish[3];
+            fish[3] = fish[4];
+            fish[4] = fish[5];
+            fish[5] = fish[6];
+            fish[6] = fish[7];
+            fish[7] = fish[8];
+            fish[8] = fish[9];
+            fish[9] = 0;
+            days++;
+        }
+        long total = 0;
+        for(int i=0; i<fish.length; i++){
+            total += fish[i];
+        }
+        System.out.println(total);
+    }
+}
