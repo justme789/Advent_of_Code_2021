ebb0454ef2a56dd62b54d053d353a5719fd63676
Abouelseoud, Ibrahim (k1893464) <nobodyxoxo123@gmail.com>
2021-12-07 05:43:24 +0000

    Day 7 Solutions

---
+++ AoC_7_b.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AoC_7_b {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Double> vals = new ArrayList<Double>();
+        Scanner textfile = new Scanner(new File("aoc.txt"));
+        while (textfile.hasNextLine()) {
+            String line = textfile.nextLine();
+            list.add(line);
+        }
+        System.out.println(list.toString());
+        for (int i = 0; i < list.get(0).lastIndexOf(","); i++) {
+            int indexOfComma = list.get(0).indexOf(",", i);
+            vals.add(Double.parseDouble(list.get(0).substring(i, indexOfComma)));
+            i = indexOfComma;
+        }
+        vals.add(Double.parseDouble(list.get(0).substring(list.get(0).lastIndexOf(",") + 1, list.get(0).length())));
+        double val = 0;
+        double sum = 99999999;
+        for (int i = 0; i < 2000; i++) {
+            double temp = 0;
+            for (int j = 0; j < vals.size(); j++){
+                val = (Math.abs(vals.get(j) - i)/2)*(1+Math.abs(vals.get(j) - i));
+                temp+= val;
+            }
+            if(temp < sum) {
+                sum = temp;
+            }
+        }
+        System.out.println(sum);
+    }
+}
